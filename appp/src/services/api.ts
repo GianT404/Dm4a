480466d6fc9c87c25c4d8310cb4165bd10c91c4d
GianT404 <longgiap.qng@gmail.com>
2025-12-31 21:56:29 +0700

    update API_URL

--- appp/src/services/api.ts
+++ appp/src/services/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-// 1. Khai báo URL Server (Thay IP nếu đổi VPS)
-export const API_URL = 'http://192.168.1.135:3000/api';
+export const API_URL = 'http://136.112.211.28:3000/api';
 
-// 2. Tạo instance axios (Cái mà ông thiếu nè)
+
 const api = axios.create({
   baseURL: API_URL,
   timeout: 10000, // Timeout 10s cho chắc
